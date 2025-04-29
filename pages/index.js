4689c551651156e4c82541205b1a1d2bb053fdad
Raphael Chetrit <raphaelchetrit@Raphaels-MacBook-Pro.local>
2025-04-29 18:47:55 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Monetization MVP</h1>
+      <p>This is your starting point for building your product.</p>
+    </div>
+  );
+}
