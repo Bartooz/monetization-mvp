23fc509bdf5ef8dbcd61cc7de90a1d703f4fd7ce
Raphael Chetrit <raphaelchetrit@Raphaels-MacBook-Pro.local>
2025-05-06 21:16:16 +0300

    Fix FullCalendar style imports for Vercel deployment

--- pages/calendar.js
+++ pages/calendar.js
@@ -4,8 +4,9 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import EventModal from "../components/EventModal";
-import "@fullcalendar/daygrid/index.css";
-import "@fullcalendar/timegrid/index.css";
+import '@fullcalendar/common/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
 
 export default function CalendarPage() {
   const [events, setEvents] = useState([]);
