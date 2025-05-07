155b61aa6a547675740cc7771ebc4c8ed58960ce
Raphael Chetrit <raphaelchetrit@Raphaels-MacBook-Pro.local>
2025-05-07 14:58:43 +0300

    Fix navigation layout in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
-import "../styles/globals.css";
-import { useEffect } from "react";
+// pages/_app.js
+import '../styles/globals.css'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  // This is optional: helps ensure body class is removed on unmount
-  useEffect(() => {
-    return () => {
-      document.body.classList.remove("modal-open");
-    };
-  }, []);
-
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <nav style={{ backgroundColor: '#333', padding: '10px' }}>
+        <Link href="/" style={{ color: 'white', marginRight: '15px' }}>🏠 Home</Link>
+        <Link href="/calendar" style={{ color: 'white', marginRight: '15px' }}>📅 Calendar</Link>
+        <Link href="/segmentation" style={{ color: 'white', marginRight: '15px' }}>🧠 Segmentation</Link>
+        <Link href="/configuration" style={{ color: 'white', marginRight: '15px' }}>⚙️ Configuration</Link>
+        <Link href="/templates" style={{ color: 'white', marginRight: '15px' }}>📄 Templates</Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  )
 }
-
-export default MyApp;
